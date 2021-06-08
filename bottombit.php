64a2cf198e9ccf522d210f6b90638e179025ac1f
Arxan10 <84944489+Arxan10@users.noreply.github.com>
2021-06-09 09:38:36 +1200

    Php includes
    Created top and bottom bits

---
+++ bottombit.php
@@ -0,0 +1,24 @@
+       <div class="box side">
+            
+            <h2>Search Area</h2>
+            
+            <p>This is where the search area goes</p>
+        
+        </div> <!-- / side bar -->
+    
+        
+        <div class="box footer">
+            Aryan Saundh
+        </div> <!-- / footer -->  
+    
+    
+   
+    
+    </body>
+
+
+</html>
+
+
+
+
--- config.php
+++ config.php
@@ -2,4 +2,9 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define ("DB_NAME","aryansaundh_game_data")
+define ("DB_NAME","aryansaundh_game_data");
+define ("DB_USERNAME","aryansaundh");
+define ("DB_PASSWORD","ju2B8CLofkzWfS");
+define ("DB_HOST","localhost");
+
+?>
